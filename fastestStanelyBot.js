0e4ccf52a9555939560b5ab1762f936e966cef49
William <wmayhood@outlook.com>
2025-10-05 22:34:47 -0500

    created a graphql post request bypassing overhead of puppeteer

--- fastestStanelyBot.js
+++ fastestStanelyBot.js
@@ -2,17 +2,101 @@ const puppeteer = require("puppeteer-extra");
 //imports puppeteer plugin, makes it harder for websites to detect what is going on
 const StealthPlugin = require("puppeteer-extra-plugin-stealth");
 
+puppeteer.use(StealthPlugin());
+
 const product_url =
-  "https://www.stanley1913.com/products/stanley-x-jennie-quencher-luxe-tumbler-30-oz?variant=53973488009576";
+  "https://www.stanley1913.com/products/winterscape-quencher-h2-0-flowstate-tumbler-40-oz?variant=44559799746687";
+
+const SHOPIFY_GRAPHQL_URL =
+  "https://stanley-pmi.myshopify.com/api/2025-01/graphql.json";
+
+const STORE_ACCESS_TOKEN = "eecaa4fbf8df42ffe25fac400b1ce513";
+
+const PRODUCT_VARIENT_ID = "gid://shopify/ProductVariant/44559799746687";
 
 async function getPage() {
   const browser = await puppeteer.launch({ headless: false });
 
   const page = await browser.newPage();
 
-  return page;
+  return { browser, page };
 }
 
-function run() {
-  const page = getPage();
+async function productRequest() {
+  try {
+    const response = await fetch(SHOPIFY_GRAPHQL_URL, {
+      method: "POST",
+      headers: {
+        accept: "application/json",
+        "content-type": "application/json",
+        "x-shopify-storefront-access-token": STORE_ACCESS_TOKEN,
+      },
+      body: JSON.stringify({
+        query: `
+          query getCartVariants($ids: [ID!]!) {
+            nodes(ids: $ids) {
+              ... on ProductVariant {
+                id
+                price {
+                  amount
+                  currencyCode
+                }
+                compareAtPrice {
+                  amount
+                  currencyCode
+                }
+                metafield(namespace: "custom", key: "minmax_ignite") {
+                  key
+                  value
+                }
+                product {
+                  metafield(namespace: "minmaxify", key: "limits") {
+                    key
+                    value
+                  }
+                }
+              }
+            }
+          }
+        `,
+        variables: {
+          ids: [PRODUCT_VARIENT_ID],
+        },
+      }),
+    });
+    const data = await response.json();
+    console.log("Shopify GraphQL Response: ");
+    console.log(JSON.stringify(data, null, 2));
+
+    return data;
+  } catch (error) {
+    console.error("Shopify GraphQL Error:", error);
+  }
 }
+
+async function run() {
+  // const { browser, page } = await getPage();
+
+  const productInfo = productRequest();
+
+  // await browser.close();
+}
+
+run();
+
+// fetch("https://stanley-pmi.myshopify.com/api/2025-01/graphql.json", {
+//   headers: {
+//     accept: "application/json",
+//     "content-type": "application/json",
+//     "sec-ch-ua":
+//       '"Not;A=Brand";v="99", "Google Chrome";v="139", "Chromium";v="139"',
+//     "sec-ch-ua-mobile": "?0",
+//     "sec-ch-ua-platform": '"macOS"',
+//     "x-sdk-variant": "storefront-api-client",
+//     "x-sdk-version": "1.0.2",
+//     "x-shopify-storefront-access-token": "eecaa4fbf8df42ffe25fac400b1ce513",
+//     Referer: "https://www.stanley1913.com/",
+//   },
+//   body: '{"query":"\\n    query getCartVariants($ids: [ID!]!) {\\n      nodes(ids: $ids) {\\n        ... on ProductVariant {\\n          id\\n          price {\\n            amount\\n            currencyCode\\n          }\\n          compareAtPrice {\\n            amount\\n            currencyCode\\n          }\\n          metafield(namespace: \\"custom\\", key: \\"minmax_ignite\\") {\\n            key\\n            value\\n          }\\n          product {\\n            metafield(namespace: \\"minmaxify\\", key: \\"limits\\") {\\n              key\\n              value\\n            }\\n          }\\n        }\\n      }\\n    }\\n  ","variables":{"ids":["gid://shopify/ProductVariant/44559799746687"]}}',
+//   method: "POST",
+// });

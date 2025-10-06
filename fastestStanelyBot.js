5efaaf149a470dbefa41c85649aab2d50510096e
William <wmayhood@outlook.com>
2025-10-05 23:26:37 -0500

    processed shopify cart request and added item to cart using graphql

--- fastestStanelyBot.js
+++ fastestStanelyBot.js
@@ -1,17 +1,18 @@
-const puppeteer = require("puppeteer-extra");
-//imports puppeteer plugin, makes it harder for websites to detect what is going on
-const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+import fetch from "node-fetch";
+import open from "open";
 
-puppeteer.use(StealthPlugin());
+//const puppeteer = require("puppeteer-extra");
+//imports puppeteer plugin, makes it harder for websites to detect what is going on
+//const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+
+//puppeteer.use(StealthPlugin());
 
 const product_url =
   "https://www.stanley1913.com/products/winterscape-quencher-h2-0-flowstate-tumbler-40-oz?variant=44559799746687";
 
 const SHOPIFY_GRAPHQL_URL =
   "https://stanley-pmi.myshopify.com/api/2025-01/graphql.json";
-
 const STORE_ACCESS_TOKEN = "eecaa4fbf8df42ffe25fac400b1ce513";
-
 const PRODUCT_VARIENT_ID = "gid://shopify/ProductVariant/44559799746687";
 
 async function getPage() {
@@ -21,8 +22,8 @@ async function getPage() {
 
   return { browser, page };
 }
-
-async function productRequest() {
+//helper function
+async function shopifyRequest(query, variables = {}) {
   try {
     const response = await fetch(SHOPIFY_GRAPHQL_URL, {
       method: "POST",
@@ -31,53 +32,59 @@ async function productRequest() {
         "content-type": "application/json",
         "x-shopify-storefront-access-token": STORE_ACCESS_TOKEN,
       },
-      body: JSON.stringify({
-        query: `
-          query getCartVariants($ids: [ID!]!) {
-            nodes(ids: $ids) {
-              ... on ProductVariant {
-                id
-                price {
-                  amount
-                  currencyCode
-                }
-                compareAtPrice {
-                  amount
-                  currencyCode
-                }
-                metafield(namespace: "custom", key: "minmax_ignite") {
-                  key
-                  value
-                }
-                product {
-                  metafield(namespace: "minmaxify", key: "limits") {
-                    key
-                    value
-                  }
-                }
-              }
-            }
-          }
-        `,
-        variables: {
-          ids: [PRODUCT_VARIENT_ID],
-        },
-      }),
+      body: JSON.stringify({ query, variables }),
     });
     const data = await response.json();
-    console.log("Shopify GraphQL Response: ");
-    console.log(JSON.stringify(data, null, 2));
-
     return data;
-  } catch (error) {
-    console.error("Shopify GraphQL Error:", error);
+  } catch (e) {
+    console.log("Shopify request error" + e);
   }
 }
 
-async function run() {
-  // const { browser, page } = await getPage();
+async function addProductToCart() {
+  const query = `mutation CreateCart($lines: [CartLineInput!]!) {
+    cartCreate(input: { lines: $lines }) {
+      cart {
+        id
+        checkoutUrl
+        lines(first: 5) {
+          edges {
+            node {
+              merchandise {
+                ... on ProductVariant {
+                  id
+                  title
+                }
+              }
+              quantity
+            }
+          }
+        }
+      }
+    }
+  }`;
+  const variables = {
+    lines: [{ merchandiseId: PRODUCT_VARIENT_ID, quantity: 1 }],
+  };
+  const data = await shopifyRequest(query, variables);
+  const cart = data.data.cartCreate.cart;
 
-  const productInfo = productRequest();
+  console.log("Cart created", JSON.stringify(cart, null, 2));
+  return cart;
+}
+
+async function run() {
+  try {
+    console.log("Starting Shopify cart flow...");
+    const cart = await addProductToCart();
+
+    //open checkout in browser
+    const checkoutUrl = cart.checkoutUrl;
+    console.log(`Opening checkout page:\n${checkoutUrl}`);
+    await open(checkoutUrl);
+  } catch (error) {
+    console.error("Error in shopify flow" + error);
+  }
 
   // await browser.close();
 }
